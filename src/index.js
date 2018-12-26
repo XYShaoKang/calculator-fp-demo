88aa0d755990460e676f95b2fbba7fe074462578
XYShaoKang <zengwenjie03@gmail.com>
2018-12-26 22:51:29 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery'
+import _ from 'ramda'
+import './styles.less'
+
+var $root = $('<div id="root" />')
+$('body').prepend($root)
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+
+const smp = new SpeedMeasurePlugin()
+
+module.exports = smp.wrap({
+  entry: {
+    app: './src/index.js',
+  },
+  devtool: 'inline-source-map', // 映射源代码
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      favicon: 'src/favicon.ico',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development', // 提高编译性能
+})
